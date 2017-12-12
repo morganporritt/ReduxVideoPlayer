ac475efbec6032e2805b499f242ddb48def49a1f
morgan <morganporritt@gmail.com>
2017-12-12 14:28:29 -0700

    updates for first section

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,3 +1,37 @@
 /**
  * Created by mporritt on 12/8/17.
  */
+
+import React, { Component } from 'react';
+
+// Functional component
+//
+// const SearchBar = () => {
+//     return <input/> // React.createElement
+// };
+
+
+// class based component
+class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {term: ''};
+    }
+
+    render() {
+        // return <input onChange={event => console.log(event.target.value)} />;
+        return (
+            <div>
+                <input
+                    value={this.state.term}
+                    onChange={event => this.setState({ term: event.target.value })} />
+            </div>
+        );
+    }
+}
+
+
+// create an instance of the class
+// new SearchBar()
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyDAWMHgUueM2FhCSB9R9510HRlI_iyo_3g';
 
 // Create a new component. This component should produce some HTML
     const App = () => {
-        return <div>Hi!</div>
-    }
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    };
 
 
 // Take this component generated HTML and put it on the page (In the DOM)
